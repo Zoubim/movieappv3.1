d3ce041d34406383a964eeeb66eb66ce99819d28
[Mohannad Alzubi] <[zoubim@live.com]>
2026-09-10 12:08:47 +1000

    Added movie list JS file

---
+++ js/movie-list.js
@@ -0,0 +1,79 @@
+/**
+ * @file movie-list.js
+ * @description This file holds the class definition of our MovieList class
+ * cSpell: ignore anytext
+ * @author M Alzubi
+ * @version 2.0.0
+ * @since vs
+ * getRow()
+ */
+
+
+/**
+ * MovieList Class
+ * This class has two properties and numerous methods
+ * Look at the Readme.md file for a fill list of methods
+ * @class MovieList
+ * @property {string} rootId - This is the ID of the HTML element where the list is to be displayed
+ * @property {Array} movieList - The array of movies to be displayed (and stored)
+ * @property {function} refresh - This method to remove all current movies from the HTML document 
+ * and display the current movie list
+ * 
+ */
+
+class MovieList {
+    constructor(rootId, movies) {
+        this.rootId = rootId; // The HTML ID where the list is going
+        this.movieList = movies; // This is the array of movies to be displayed
+        this.refresh();
+    }
+    // Methods
+
+    /**
+     * @function movieRow
+     */
+
+    /**
+     * @function genMovieList
+     */
+
+    /**
+     * @function genMovieListSearchList
+     **/
+
+    /** 
+     * @function removeElements
+     *  */
+
+    /** 
+     * @function getRow
+      */
+
+     /**
+      * @function refresh
+      */
+
+     /**
+      * @function add
+      */
+
+     /** 
+      * @function update
+      */
+
+     /**
+      * @function delete
+      */
+
+     /**
+      * @function sortA2Z
+      */
+
+     /**
+      * @function sortZ2A
+      */
+     
+     /**
+      * @function search
+      */
+}
